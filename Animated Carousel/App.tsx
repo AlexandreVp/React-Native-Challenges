167c891c4c9ac5c4e1a85075179503f43d7cebed
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-12-16 13:27:13 -0300

    Revert all recent changes

--- Animated Carousel/App.tsx
+++ Animated Carousel/App.tsx
@@ -24,12 +24,6 @@ const {
 const CARD_PADDING = 50;
 const CARD_WIDTH = screenWidth - (CARD_PADDING * 2);
 
-const teste = 1;
-
-const t2 = 2;
-
-const t3 = 3;
-
 export default function App() {
 
 	const offsetX = useValue(0);

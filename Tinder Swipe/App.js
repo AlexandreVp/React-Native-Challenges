e8d39259b7c7cf386318666e142dba6233165162
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-01-09 22:43:01 -0300

    Adding the upper image

--- Tinder Swipe/App.js
+++ Tinder Swipe/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions, SafeAreaView, Platform } from 'react-native';
-import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue, interpolate } from 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import AnimatedCard from './src/components/AnimatedCard';
+import CrossSVG from './src/components/CrossSVG';
+import HeartSVG from './src/components/HeartSVG';
 import { images } from './src/utils/Helpers';
 
 
@@ -16,12 +19,55 @@ export default function App() {
 	const [locked, setLocked] = useState(0);
 
 	const [index, setIndex] = useState(0);
-	const [nextIndex, setNextIndex] = useState(index + 1);
+	const [secondIndex, setSecondIndex] = useState(index + 1);
 
+	const scale = useSharedValue(1);
+	const overrideNopeOpacity = useSharedValue(0);
+	const overrideLikeOpacity = useSharedValue(0);
+
+	const x = useSharedValue(0);
+	const y = useSharedValue(0);
+	const originY = useSharedValue(0);
+
+	const upperImageStyle = useAnimatedStyle(() => {
+		let factor = -1;
+
+		// half of the screen's height changes the rotation direction
+		if (originY.value < SCREEN_HEIGHT / 2) {
+			factor = 1;
+		}
+
+		// the further we are to the left (-) or right (+), we rotate by up to 10deg
+		const rotateZ = interpolate(
+			x.value, 
+			[0, factor * rangeThreshold], 
+			[0, 10]
+		);
+
+		// the image rotation with border radius is not working well on android, thus disabled
+		return {
+			elevation: 2,
+			zIndex: 2,
+			top: 0,
+			right: 0,
+			bottom: 0,
+			left: 0,
+			position: 'absolute',
+			alignItems: 'center',
+			justifyContent: 'center',
+			transform: [
+				{ scale: scale.value },
+				{ translateX: x.value },
+				{ translateY: y.value },
+				{ rotateZ: Platform.OS === 'android' ? '0deg' : `${rotateZ}deg` },
+			],
+		};
+	});
 
 	const lowerImageStyle = useAnimatedStyle(() => {
 		return {
 			zIndex: -1,
+			elevation: -1,
 			top: 0,
 			right: 0,
 			bottom: 0,
@@ -43,10 +89,29 @@ export default function App() {
 
 				<AnimatedCard 
 					images={images}
-					index={nextIndex}
+					index={secondIndex}
 					style={lowerImageStyle}
 					pointerEvents='none'
 				/>
+
+				<AnimatedCard 
+					style={upperImageStyle}
+					images={images}
+					index={index}
+					pointerEvents='auto'
+				>
+
+				</AnimatedCard>
+
+				<View style={styles.headFoot}>
+					<TouchableOpacity onPress={() => {}} style={styles.icon}>
+						<CrossSVG />
+					</TouchableOpacity>
+
+					<TouchableOpacity onPress={() => {}} style={styles.icon}>
+						<HeartSVG />
+					</TouchableOpacity>
+				</View>
 				
 			</View>
 		</SafeAreaView>
@@ -64,7 +129,7 @@ const styles = StyleSheet.create({
 	},
 	container: {
 		flex: 1,
-		justifyContent: 'space-around'
+		justifyContent: 'space-around',
 	},
 	headFoot: {
 		flexDirection: 'row',
@@ -73,5 +138,15 @@ const styles = StyleSheet.create({
 		minHeight: 40,
 		marginVertical: 16,
 		borderWidth: 1
+	},
+	icon: {
+		justifyContent: 'center',
+		alignContent: 'center',
+		alignItems: 'center',
+		width: 55,
+		height: 55,
+		padding: 14,
+		borderRadius: 100,
+		backgroundColor: 'rgba(251,251,251,1)',
 	},
 });

1d9c865e92f8f63b32109026ac22233f48a7dac4
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-01-12 22:41:09 -0300

    [IMAGES CAROUSEL] Finishing images carousel

--- Images Carousel/App.js
+++ Images Carousel/App.js
@@ -2,44 +2,41 @@
 // Illustrations by: SAMji https://dribbble.com/SAMji_illustrator
 
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, Dimensions, Image, ImageBackground } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Dimensions, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { PanGestureHandler, ScrollView, FlatList } from 'react-native-gesture-handler';
-import Animated, { 
-	useAnimatedGestureHandler, 
+import { FlatList } from 'react-native-gesture-handler';
+import Animated, {
 	useSharedValue, 
 	useAnimatedStyle, 
-	withSpring,
 	interpolate,
-	Extrapolate,
-	runOnJS,
 	useAnimatedScrollHandler
 } from 'react-native-reanimated';
 
 
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
-
-const AnimatedFlatlist = Animated.createAnimatedComponent(FlatList);
-
 const data = [
-    'https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200',
+	'https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200',
     'https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200'
-
+	
 ];
 
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 const imageWidth = SCREEN_WIDTH * 0.7;
 const imageHeight = SCREEN_WIDTH * 1.1;
 
+const AnimatedFlatlist = Animated.createAnimatedComponent(FlatList);
+
 export default function App() {
 
+	const scrollX = React.useRef(useSharedValue(0)).current;
 
 	const onScrollEvent = useAnimatedScrollHandler((event) => {
-		console.log(event.contentOffset.x);
+		scrollX.value = event.contentOffset.x;
+		console.log(scrollX.value);
 	});
 
 	return (
@@ -48,11 +45,22 @@ export default function App() {
 			<View style={styles.backgroundImageContainer}>
 				{
 					data.map((value, index) => {
+
+						const backImageStyle = useAnimatedStyle(() => {
+							return {
+								opacity: interpolate(
+									scrollX.value,
+									[(index-1)*SCREEN_WIDTH, index*SCREEN_WIDTH, (index+1)*SCREEN_WIDTH],
+									[0, 1, 0]
+								)
+							}
+						})
+
 						return (
-							<Image 
+							<Animated.Image 
 								key={`image${index}`}
 								source={{ uri: value }}
-								style={styles.backgroundImage}
+								style={[styles.backgroundImage, backImageStyle]}
 								blurRadius={12}
 							/>
 						)
@@ -72,23 +80,8 @@ export default function App() {
 					)
 				}}
 				onScroll={onScrollEvent}
+				showsHorizontalScrollIndicator={false}
 			/>
-			{/* <FlatList 
-				data={data}
-				keyExtractor={(_, index) => index.toString()}
-				horizontal
-				pagingEnabled
-				renderItem={(item) => {
-					return (
-						<View>
-							<Image source={{ uri: item }} style={styles.image} />
-						</View>
-					)
-				}}
-			/>
-			<ScrollView 
-					
-			/> */}
 		</SafeAreaView>
 	);
 }
@@ -101,15 +94,15 @@ const styles = StyleSheet.create({
 	itemContainer: {
 		width: SCREEN_WIDTH,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
 	},
 	image: {
 		width: imageWidth,
 		height: imageHeight,
-		borderRadius: 16,
+		borderRadius: 16
 	},
 	backgroundImageContainer: {
-		...StyleSheet.absoluteFillObject
+		...StyleSheet.absoluteFillObject,
 	},
 	backgroundImage: {
 		...StyleSheet.absoluteFillObject

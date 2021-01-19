3f04fb9bd469b697eb783684a26d7d14b0b414f7
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-01-18 23:24:05 -0300

    [ACCORDION MENU] Layout of the menus

--- Accordion Menu/App.js
+++ Accordion Menu/App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import data from './Utils/data';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+	return (
+		<View style={styles.container}>
+			<StatusBar hidden />
+			{data.map(({bg, color, category, subCategories}) => {
+				return (
+					<TouchableOpacity 
+						key={category} 
+						onPress={() => {}} 
+						style={styles.cardContainer}
+						activeOpacity={0.9}
+					>
+						<View style={[styles.card, { backgroundColor: bg }]}>
+							<Text style={[styles.heading, { color: color }]}>{category}</Text>
+							<View style={styles.subCategoriesList}>
+								{subCategories.map(subCategory => {
+									return (
+										<Text key={subCategory} style={[styles.subCategory, { color }]}>{subCategory}</Text>
+									)
+								})}
+							</View>
+						</View>
+					</TouchableOpacity>
+				)
+			})}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+	},
+	cardContainer: {
+		flexGrow: 1
+	},
+	card: {
+		flexGrow: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	heading: {
+		fontSize: 38,
+		fontWeight: 'bold',
+		textTransform: 'uppercase',
+		letterSpacing: -2
+	},
+	subCategory: {
+		fontSize: 20,
+		lineHeight: 20 * 1.5,
+		textAlign: 'center'
+	}
 });
---
+++ Accordion Menu/Utils/data.js
@@ -0,0 +1,64 @@
+export default [
+    {
+        bg: '#a8dde9',
+        color: '#3f5b98',
+        category: 'Healthcare',
+        subCategories: ['Skincare', 'Personal care', 'Health', 'Eye care'],
+    },
+    {
+        bg: '#086e4b',
+        color: '#fcbe4a',
+        category: 'Food & Drink',
+        subCategories: [
+            'Fruits and Vegetables',
+            'Frozen food',
+            'Bakery',
+            'Snacks & Desserts',
+            'Beverages',
+            'Alcoholic beverages',
+            'Noodles & Pasta',
+            'Rice & Cooking oil'
+        ],
+    },
+    {
+        bg: '#fecbca',
+        color: '#ed5963',
+        category: 'Beauty',
+        subCategories: [
+            'Skincare',
+            'Makeup',
+            'Nail Care',
+            'Perfume',
+        ],
+    },
+    {
+        bg: '#89cff0',
+        color: '#f0aa89',
+        category: 'Baby & Kids',
+        subCategories: [
+            'Toys',
+            'LEGO',
+            'Eletronics',
+            'Puzzles',
+            'Costumes',
+            'Food',
+            'Hygiene & Care',
+            'Child\'s room'
+        ],
+    },
+    {
+        bg: '#69411b',
+        color: '#ff6500',
+        category: 'Homeware',
+        subCategories: [
+            'Fruits and Vegetables',
+            'Frozen food',
+            'Bakery',
+            'Snacks & Desserts',
+            'Beverages',
+            'Alcoholic beverages',
+            'Noodles & Pasta',
+            'Rice & Cooking oil'
+        ],
+    }
+]

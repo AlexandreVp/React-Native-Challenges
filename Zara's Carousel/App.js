60f656f642496eea864bf72025b1854dc1cbb5e7
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-01-19 22:04:48 -0300

    [ZARA'S CAROUSEL] Adding pagination circles

--- Zara's Carousel/App.js
+++ Zara's Carousel/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Image, FlatList, View, StatusBar, Dimensions, StyleSheet, SafeAreaView } from 'react-native';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedScrollHandler } from 'react-native-reanimated';
 
 const {width, height} = Dimensions.get('screen');
 
 const ITEM_WIDTH = width;
 const ITEM_HEIGHT = height * .75;
+const DOT_SIZE = 8;
+const DOT_SPACING = 8;
+const DOT_INDICATOR_SIZE = DOT_SIZE + DOT_SPACING;
 
 const images = [
     'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_1_1_1.jpg?ts=1606727905128',
@@ -24,15 +27,21 @@ const product = {
     price: '29.99£'
 }
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
 export default () => {
 
     const scrollY = useSharedValue(0);
+
+    const onScrollEvent = useAnimatedScrollHandler((event) => {
+        scrollY.value = event.contentOffset.y;
+    });
 	
     return (
 		<SafeAreaView>
 			<StatusBar hidden/>
             {/* <View style={styles.flatlistWrapper}> */}
-                <FlatList
+                <AnimatedFlatList
                     data={images}
                     style={styles.flatlistWrapper}
                     keyExtractor={(_, index) => index.toString()}
@@ -43,12 +52,26 @@ export default () => {
                             </View>
                         )
                     }}
+                    onScroll={onScrollEvent}
                     snapToInterval={ITEM_HEIGHT}
                     decelerationRate='fast'
                     showsVerticalScrollIndicator={false}
                     bounces={false}
                 />
             {/* </View> */}
+            <View style={styles.pagination}>
+                {images.map((_, index) => {
+                    return (
+                        <View 
+                            key={index.toString()}
+                            style={styles.dot}
+                        />
+                    )
+                })}
+                <View
+                    style={styles.dotIndicator}
+                />
+            </View>
 		</SafeAreaView>
 	)
 }
@@ -61,5 +84,27 @@ const styles = StyleSheet.create({
     flatlistWrapper: {
         height: ITEM_HEIGHT,
         overflow: 'hidden'
+    },
+    pagination: {
+        position: 'absolute',
+        alignItems: 'center',
+        top: ITEM_HEIGHT/2 - 30,
+        left: 20,
+    },
+    dot: {
+        width: DOT_SIZE,
+        height: DOT_SIZE,
+        borderRadius: DOT_SIZE/2,
+        backgroundColor: '#333',
+        marginBottom: DOT_SPACING
+    },
+    dotIndicator: {
+        width: DOT_INDICATOR_SIZE,
+        height: DOT_INDICATOR_SIZE,
+        borderRadius: DOT_INDICATOR_SIZE/2,
+        borderWidth: 1,
+        borderColor: '#333',
+        position: 'absolute',
+        top: -DOT_SIZE/2,
     }
 })

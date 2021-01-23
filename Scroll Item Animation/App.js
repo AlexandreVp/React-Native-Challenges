f69f53777adb46977c0f55e8476d3f8909f46190
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-01-23 10:48:03 -0300

    [SCROLL ITEM ANIMATION] Adding FlatList and Background Image

--- Scroll Item Animation/App.js
+++ Scroll Item Animation/App.js
@@ -16,16 +16,76 @@ const DATA = [...Array(30).keys()].map((_, i) => {
         email: faker.internet.email(),
     };
 });
+const BG_IMAGE = 'https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg?auth';
 
 export default () => {
 	
     return (
-		<View style={{flex: 1, backgroundColor: '#fff'}}>
+		<SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
 			<StatusBar hidden/>
-		</View>
+            <Image 
+                source={{ uri: BG_IMAGE }}
+                style={[StyleSheet.absoluteFillObject]}
+                blurRadius={50}
+            />
+            <FlatList 
+                data={DATA}
+                keyExtractor={item => item.key}
+                contentContainerStyle={{ padding: SPACING, paddingTop: StatusBar.currentHeight || 42 }}
+                renderItem={({item, index}) => {
+                    return (
+                        <View style={styles.profileWrapper}>
+                            <Image 
+                                source={{ uri: item.image }}
+                                style={styles.avatar}
+                            />
+                            <View>
+                                <Text style={styles.name}>{item.name}</Text>
+                                <Text style={styles.jobTitle}>{item.jobTitle}</Text>
+                                <Text style={styles.email}>{item.email}</Text>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
-
+    profileWrapper: {
+        flexDirection: 'row',
+        padding: SPACING,
+        marginBottom: SPACING,
+        alignItems: 'center',
+        backgroundColor: 'rgb(250, 250, 250)',
+        borderRadius: 16,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 10
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 20,
+        elevation: 5
+    },
+    avatar: {
+        width: AVATAR_SIZE,
+        height: AVATAR_SIZE,
+        borderRadius: AVATAR_SIZE/2,
+        marginRight: SPACING/2
+    },
+    name: {
+        fontSize: 22,
+        fontWeight: '700',
+    },
+    jobTitle: {
+        fontSize: 18,
+        opacity: .7,
+    },
+    email: {
+        fontSize: 14,
+        opacity: .8,
+        color: '#0099cc'
+    }
 });

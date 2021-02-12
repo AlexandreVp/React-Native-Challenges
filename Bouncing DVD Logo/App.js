ae384a40934ff1fa0c9347bf033fdd0c973f1fb0
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-02-12 13:46:39 -0300

    [BOUNCING DVD LOGO] Animating the Logo colors

--- Bouncing DVD Logo/App.js
+++ Bouncing DVD Logo/App.js
@@ -1,41 +1,73 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { View, StyleSheet, Text, Dimensions } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming, withRepeat, Easing } from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming, withRepeat, Easing, runOnJS } from 'react-native-reanimated';
  
 const { width, height } = Dimensions.get('window');
 const LOGO_WIDTH = 122;
 const LOGO_HEIGHT = 80;
-const DURATION = 3 * 1000;
+const DURATION = Math.PI / 2 * 1000;
+const COLORS = [
+	'#ff0000',
+	'#00ff00',
+	'#0000ff',
+	'#ffff00',
+	'#00ffff',
+	'#ff00ff'
+];
 
-const Logo = () => {
+const Logo = ({ color }) => {
+
+	const styleVIDEO = useAnimatedStyle(() => {
+		return {
+			color: color.value
+		}
+	})
+
+	const styleDVD = useAnimatedStyle(() => {
+		return {
+			color: color.value
+		}
+	})
+
 	return (
 		<View style={styles.logoWrapper}>
-			<Text style={styles.dvdText}>DVD</Text>
-			<Text style={styles.videoText}>VIDEO</Text>
+			<Animated.Text style={[styles.dvdText, styleDVD]}>DVD</Animated.Text>
+			<Animated.Text style={[styles.videoText, styleVIDEO]}>VIDEO</Animated.Text>
 		</View>
 	)
 };
 
 const App = () => {
 
+	const color = useSharedValue(COLORS[0]);
 	const translateX = useSharedValue(0);
 	const translateY = useSharedValue(0);
+
+	const onBounce = useCallback(() => {
+		"worklet"
+		const colorsLeft = COLORS.concat();
+
+		colorsLeft.splice(colorsLeft.indexOf(color.value), 1);
+
+		color.value = colorsLeft[Math.round(Math.random() * (colorsLeft.length - 1))];
+
+	}, []);
 
 	useEffect(() => {
 		translateX.value = withRepeat(
 			withTiming(width - LOGO_WIDTH, {
 				duration: DURATION,
 				easing: Easing.linear
-			}),
+			}, onBounce),
 			-1,
 			true
 		);
 		translateY.value = withRepeat(
 			withTiming(height - LOGO_HEIGHT, {
-				duration: DURATION * 1.6,
+				duration: DURATION * Math.PI,
 				easing: Easing.linear
-			}),
+			}, onBounce),
 			-1,
 			true
 		)
@@ -58,7 +90,7 @@ const App = () => {
 		<View style={styles.container}>
 			<StatusBar hidden/>
 			<Animated.View style={[style]}>
-				<Logo />
+				<Logo color={color} />
 			</Animated.View>
 		</View>
 	);
@@ -67,7 +99,7 @@ const App = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		// backgroundColor: '#000000',
+		backgroundColor: '#000000',
 	},
 	logoWrapper: {
 		width: LOGO_WIDTH,

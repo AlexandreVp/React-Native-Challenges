7b8a0d63d9e1732d1d35104df86a7a2c7ae9d06a
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-02-12 20:01:25 -0300

    [GALLERY VIEW] Fetching images from Pexels

---
+++ Gallery View/App.js
@@ -0,0 +1,73 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+    StatusBar,
+    FlatList,
+    Image,
+    Animated,
+    Text,
+    View,
+    Dimensions,
+    StyleSheet,
+    TouchableOpacity,
+    Easing,
+    SafeAreaViewBase,
+    SafeAreaView,
+} from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+
+// const API_KEY = "563492ad6f917000010000011d001c06309c45cb97482fd1e998d5d5"
+const API_KEY = '563492ad6f91700001000001b42e951419074cd0aec0976621706bd2'
+const API_URL = "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20"
+
+const fetchImagesFromPexels = async () => {
+    const data = await fetch(API_URL, {
+        headers: {
+            'Authorization': API_KEY
+        }
+    });
+
+    const { photos } = await data.json();
+
+    return photos;
+};
+
+const Loading = () => {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontWeight: 'bold', fontSize: 24}}>Loading...</Text>
+        </View>
+    )
+};
+
+export default () => {
+
+    const [images, setImages] = useState(null);
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            const images = fetchImagesFromPexels();
+
+            setImages(images);
+        };
+        fetchImages();
+    }, [])
+
+
+    if (!images) {
+        return <Loading />
+    }
+
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+        backgroundColor: '#000000'
+	}
+});

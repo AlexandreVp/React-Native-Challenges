0d6678204a65348e9d2dcf1cf53117fb68354791
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2021-02-19 20:00:39 -0300

    [PROGRESS BAR INDICATOR] Progress component

--- Progress Bar Indicator/App.js
+++ Progress Bar Indicator/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
+
+const Progress = ({ step, steps, height }) => {
+
+	return (
+		<View 
+			style={{
+				height,
+				backgroundColor: 'rgba(0,0,0,0.1)',
+				borderRadius: height,
+				overflow: 'hidden'
+			}}
+		>
+			<View 
+				style={{
+					height,
+					width: '100%',
+					backgroundColor: 'rgba(0,0,0,0.5)',
+					borderRadius: height,
+					position: 'absolute',
+					left: 0,
+					top: 0
+				}}
+			/>
+		</View>
+	)
+};
+
 export default function App() {
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<StatusBar hidden />
-			<Text style={{ fontSize: 42 }}>❤️</Text>
-			<Text
-				style={{
-					fontFamily: 'Menlo',
-					marginTop: 10,
-					fontWeight: '800',
-					fontSize: 16,
-				}}
-			>
-				Expo
-			</Text>
-			<Text style={{ fontFamily: 'Menlo', fontStyle: 'italic', fontSize: 12 }}>
-				(expo.io)
-			</Text>
+			<Progress step={1} steps={10} height={20}/>
 		</SafeAreaView>
 	);
 }
@@ -29,7 +43,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
 		justifyContent: 'center',
-	},
+		padding: 20
+	}
 });
